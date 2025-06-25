79fdaeae5a7cbe7d7537890e9e32caab45d0841d
sneha-amruth <sneha.amruth24@gmail.com>
2025-06-25 17:38:46 +0530

    Clean up: Remove unused main.py file

--- main.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def main():
-    """Runs the Streamlit app."""
-    os.system("/usr/bin/python3 -m streamlit run app.py --server.headless true")
-
-if __name__ == "__main__":
-    main()
